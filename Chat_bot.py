d45c1958e70386d84699718d670a84b3fa2e4c07
yao-lin <43307112+yao-lin-lynn@users.noreply.github.com>
2023-06-26 23:23:17 +0900

    Add multiple pages

---
+++ Chat_bot.py
@@ -0,0 +1,31 @@
+import os
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+st.title("💬 にゃんぽんりんGPT")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "どんな御用でしょうか"}]
+
+with st.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([4, 1])
+    user_input = a.text_input(
+        label="Your message:",
+        placeholder="聞きたいことを入力してください",
+        label_visibility="collapsed",
+    )
+    b.form_submit_button("送信", use_container_width=True)
+
+for msg in st.session_state.messages:
+    message(msg["content"], is_user=msg["role"] == "user")
+    
+if user_input and openai_api_key:
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    message(user_input, is_user=True)
+    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    message(msg.content)
--- pages/1_Question_test.py
+++ pages/1_Question_test.py
@@ -1,19 +1,25 @@
-import altair as alt
-import pandas as pd
-import streamlit as st
-
-import openai
 import os
+import openai
+import streamlit as st
 import logging
 import sys
 
 from llama_index import QuestionAnswerPrompt, VectorStoreIndex, SimpleDirectoryReader
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-"""
-# ハッカソンやるぞ〜
-"""
+st.title("質問テスト用")
+
+with st.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([5, 1])
+    user_input = a.text_area(
+        label="Your message:",
+        placeholder="質問を入力してください",
+        label_visibility="collapsed",
+        height=300,
+    )
+    b.form_submit_button("送信", use_container_width=True)
+
 
 documents = SimpleDirectoryReader(input_dir="./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
@@ -22,23 +28,14 @@ prompt_file = 'prompt.txt'
 with open(prompt_file, 'r', encoding='utf-8') as file:
     QA_PROMPT_TMPL = file.read()
 
-txt = st.text_area('質問を入力してください', '''
-    今日の天気を教えてください
-    ''', height=300)
-
-if st.button('にゃんぽんりんGPTに聞く'):
+if user_input and openai_api_key:
+    openai.api_key = openai_api_key
     with st.spinner('考え中...'):
         QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
         query_engine = index.as_query_engine(text_qa_template=QA_PROMPT)
-        response = query_engine.query(txt)
+        response = query_engine.query(user_input)
     for i in response.response.split("。"):
         st.write(i)
-        st.write('\n')
-
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-
-
-
-
